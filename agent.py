96441218f8b1708605651bc00a2202635ac1780a
Stefan Foulis <stefan@foulis.ch>
2013-11-15 03:42:08 +0100

    adds possibility to control what ip is reported
    * by providing a full ip
    * by providing a subnet

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 from docker import Client
 import socket
-from netaddr import IPAddress
+from netaddr import IPAddress, IPNetwork
 import redis
 import json
 import time
@@ -11,18 +11,27 @@ import netifaces
 url = urlparse.urlparse(os.environ.get('REDIS_URL', 'redis://localhost:6379/'))
 pool = redis.ConnectionPool(host=url.hostname, port=url.port, db=0, password=url.password)
 redis_connection = redis.StrictRedis(connection_pool=pool)
+private_ip = os.environ.get('PRIVATE_IP', None)
+if private_ip:
+    private_ip = IPAddress(private_ip)
+private_ip_subnet = os.environ.get('PRIVATE_IP_SUBNET', None)
+if private_ip_subnet:
+    private_ip_subnet = IPNetwork(private_ip_subnet)
 
 d = Client()
 
 
 def get_private_ip_or_hostname():
+    if private_ip:
+        return str(private_ip)
     for iface in netifaces.interfaces():
         if iface.startswith("eth"):
             for address_list in netifaces.ifaddresses(iface).values():
                 for address_dict in address_list:
                     if not ":" in address_dict['addr']:
                         if IPAddress(address_dict['addr']).is_private():
-                            return address_dict['addr']
+                            if not private_ip_subnet or IPAddress(address_dict['addr']) in private_ip_subnet:
+                                return address_dict['addr']
     return socket.gethostname()
 
 
@@ -33,5 +42,6 @@ while True:
         app_name = container['Image']
         if 'app/' in app_name and ':latest' in app_name:
             apps.append([app_name.replace("app/", "").replace(":latest",""), container['Ports'].split("->")[0]])
+    print "reporting %s" % json.dumps({get_private_ip_or_hostname(): apps})
     redis_connection.publish("app_announce", json.dumps({get_private_ip_or_hostname(): apps}))
     time.sleep(10)
